d7f9e692425689f77deb615b6fb24109665f699b
conashin <41155840+conashin@users.noreply.github.com>
2025-02-23 14:59:38 +0800

    Lab1: Add mode 10 and 11

--- Lab1/main.v
+++ Lab1/main.v
@@ -24,4 +24,36 @@ module mode01(
     assign ans = numans;
 endmodule
 
-    
+module mode10(
+    input [2:0]x,
+    input [2:0]y,
+    output [7:0]ans
+);
+    integer shifting;
+    assign shifting = y;
+    assign ans = x << shifting;
+endmodule
+
+module mode11(
+    input [2:0]x,
+    input [2:0]y,
+    output [7:0]ans
+);
+    integer shifting;
+    assign shifting = x;
+    assign ans = y >> shifting;
+endmodule
+
+module calulator(
+    input [1:0]mode,
+    input [2:0]x,
+    input [2:0]y,
+    output [7:0]ans
+);
+    wire [7:0]ans;
+    mode00 mode00(x, y, ans);
+    mode01 mode01(x, y, ans);
+    mode10 mode10(x, y, ans);
+    mode11 mode11(x, y, ans);
+endmodule
+
