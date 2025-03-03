b546f66e4660bd327e12d076d69f1ac79462fdc7
conashin <{ID}+{username}@users.noreply.github.com>
2025-03-03 05:06:02 +0000

    Lab2: 新增4-bit 7-segment顯示器接口及LED控制器位置輸出

--- Lab2/lab2_initial_pos_ok.v
+++ Lab2/lab2_initial_pos_ok.v
@@ -5,6 +5,11 @@ module Top_Module (
 );
     wire slow_clk;       // 分頻後的時鐘
 
+
+    wire [3:0] DK1, DK2, DK3, DK4; // 4-bit 7-segment display
+
+    assign DK1 = SW[6:3];
+    
     // 實例化時鐘分頻模組
     clock_divider clk_div (
         .clk(clk),
@@ -24,9 +29,10 @@ endmodule
 module LED_Controller (
     input [7:0] SW,       // 8-bit 控制開關
     input clk,            // 來自 clock_divider 的慢時鐘
-    output reg [15:0] LED // 16 顆 LED
+    output reg [15:0] LED, // 16 顆 LED
+    output reg [3:0] position // LED 當前位置 (範圍: 0~15)
 );
-    reg [3:0] position;   // LED 當前位置 (範圍: 0~15)
+    // reg [3:0] position;   // LED 當前位置 (範圍: 0~15)
     wire move_mode;       // 0: 移動 1 顆 LED, 1: 移動 2 顆 LED
     wire light_mode;      // 0: 亮燈模式, 1: 熄滅模式
 
@@ -37,8 +43,7 @@ module LED_Controller (
         if (SW[0]) begin // Reset
             // *重置 LED 狀態*
             LED = (light_mode) ? 16'b1111_1111_1111_1111 : 16'b0000_0000_0000_0000;
-            position 
-            = SW[6:3];  // 設置初始 LED 位置
+            position = SW[6:3];  // 設置初始 LED 位置
             LED[position] = (light_mode) ? 1'b0 : 1'b1;
         end else begin
             // *累積 LED 亮燈狀態*
